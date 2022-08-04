bf88a70fa1048f147cbb5bd6af437ac394fdb733
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-08-04 14:36:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+function   moveDodgerLeft(){
+    let dodger = document.getElementById('dodger')
+    dodger.style.left = "0px"
+    dodger.style.backgroundColor = "#FF69B4";
+}
+
+
+let leftNumbers = dodger.style.left.replace('px', '');
+let left = parseInt(leftNumbers, 10);
+function moveDodgerRight() {
+    if ( left < 360) {
+      dodger.style.left = `${left + 4}px`;
+    }
+  }
